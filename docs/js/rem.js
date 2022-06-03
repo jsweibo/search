eab585c6550f578fe34c32ef6c41c7524e3ba203
jsweibo <3546705775@qq.com>
2022-06-03 10:22:04 +0800

    publish v1.0.0

---
+++ docs/js/rem.js
@@ -0,0 +1,17 @@
+(function () {
+  function resize() {
+    if (innerWidth >= 320 && innerWidth <= 414) {
+      document.documentElement.style.fontSize = (innerWidth / 375) * 100 + 'px';
+    } else if (innerWidth > 414) {
+      document.documentElement.style.fontSize = '100px';
+    }
+  }
+
+  window.addEventListener('DOMContentLoaded', function () {
+    resize();
+  });
+
+  window.addEventListener('resize', function () {
+    resize();
+  });
+})();
